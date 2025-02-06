dc53f29a98d96fd016294442b6632bff14e1089c
shamsudeen <shamsudeenaliyu000@gmail.com>
2025-02-07 00:19:41 +0100

    Updated dashbord.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Calculator</title>
+    <link rel="stylesheet" type="text/css" href="asset\maint.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+
+
+    <section style="display:none;" id="SignUp"> 
+        <h1>Sign Up</h1>
+        <form action="register.php" method="POST">
+            <label>Full Name</label><br>
+            <input type="text" name="fname" placeholder="Enter Full Name"><br><br>
+            <label>Email</label><br>
+            <input type="email" name="email" placeholder="Enter Email"><br><br>
+            <label>Password</label><br>
+            <input type="password" name="password" placeholder="Enter Password"><br><br>
+            <label>Comform Password</label><br>
+            <input type="Password" name="Cpassword" placeholder="Comform Password"><br><br>
+            <input type="submit" value="Sign Up" class="submit">
+            <p>----------Or----------</p>
+            <p class="otheropption">Already have account <a href="#" id="otheropptionIn">Sign In</a></p>
+        </form>
+    </section>
+
+    <section id="SignIn">
+        <h1>Sign In</h1>
+        <form action="register.php" method="POST">
+            <label>Email</label><br>
+            <input type="email" name="email" placeholder="Enter Email"><br><br>
+            <label>Password</label><br>
+            <input type="password" name="password" placeholder="Enter Password"><br><br>
+            <input type="submit" value="Sign In" class="submit">
+            <p>----------Or----------</p>
+            <p class="otheropption">Don't have account <a href="#" id="otheropptionUp">Sign Up</a></p>
+        </form>
+    </section>
+
+
+
+    <script type="text/javascript" src="asset\script.js"></script>
+</body>
+
+</html>
---
+++ production.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Calculator</title>
+    <link rel="stylesheet" type="text/css" href="asset\nav.css">
+</head>
+
+<body>
+<?php  require "includes/nav.php"; ?>
+
+
+<?php  require "includes/footer.php"; ?>
+
+</body>
+</html>
+
+
